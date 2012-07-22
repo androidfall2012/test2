21e90b43b3b316fb636b7e544503229f701bc570
AndroidFall2012 <Android.Fall2012@gmail.com>
2012-07-22 14:16:34 -0700

    test2comit

---
+++ src/com/WRHEnterprises/mytest/MytestActivity.java
@@ -0,0 +1,13 @@
+package com.WRHEnterprises.mytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MytestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
